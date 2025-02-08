a30d0e9404e50495ac8125f9befbfd24d89cc382
Patricia Bernadette Aspillaga <144974708+TriciaBAA@users.noreply.github.com>
2025-02-08 10:15:47 +0000

    Migration and Model Activity

--- Project1/app/Http/Controllers/Controller.php
+++ Project1/app/Http/Controllers/Controller.php
@@ -6,3 +6,51 @@ abstract class Controller
 {
     //
 }
+
+class TaskController extends Controller
+{
+    // Display all tasks
+    public function index()
+    {
+        return response()->json(Task::all());
+    }
+
+    // Show a specific task
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    // Store a new task
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean'
+        ]);
+
+        $task = Task::create($request->all());
+        return response()->json($task, 201);
+    }
+
+    // Update a task
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean'
+        ]);
+
+        $task->update($request->all());
+        return response()->json($task);
+    }
+
+    // Delete a task
+    public function destroy(Task $task)
+    {
+        $task->delete();
+        return response()->json(null, 204);
+    }
+}
--- Project1/routes/web.php
+++ Project1/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GreetController;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,3 +13,6 @@ Route::get('/hello', function () {
 });
 
 Route::get('/greet', [GreetController::class, 'show']);
+
+Route::resource('tasks', TaskController::class);
+
