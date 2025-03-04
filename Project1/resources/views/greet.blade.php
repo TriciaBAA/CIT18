391eebd00e1f06a06bb520f317246da6154779e4
TriciaBAA <pat.aspillaga@gmail.com>
2025-03-05 01:02:32 +0800

    Installing Tailwind CSS and Updating Frontend

--- Project1/resources/views/greet.blade.php
+++ Project1/resources/views/greet.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Greeting</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hello, Laravel!</h1>
+    <h1 class="text-3xl font-bold underline">
+        Hello, Laravel!
+    </h1>
 </body>
 </html>
